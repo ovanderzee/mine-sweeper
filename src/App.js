9138fed4fd957e91007e1d767113fb6d8e5c232a
Onno van der Zee <hastalavista@dds.nl>
2021-07-31 14:19:44 +0200

    fix "The stylesheet .../fontface.css was not loaded because its MIME type, "text/html" is not "text/css" in Firefox.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import PageProvider from './store/PageProvider'
 import Paging from './components/Paging'
+import './fonts/DejaVu-Sans-fontfacekit/fontface.css'
 import './App.css'
 
 function App() {
