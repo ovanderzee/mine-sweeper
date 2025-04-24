1ea946b4ed15d860e58b0967184dc71717f29130
Onno van der Zee <hastalavista@dds.nl>
2025-04-24 21:32:32 +0200

    first unit-tests

---
+++ src/__mocks__/game-states.ts
@@ -0,0 +1,84 @@
+import { GameState, GameStages, CellStateStage} from '../common/game-types'
+
+const newGameState: GameState = {
+  "stage": GameStages.NEW,
+  "board": [
+    [
+      { "stage": CellStateStage.HIDDEN, "fill": 9, "row": 0, "col": 0 },
+      { "stage": CellStateStage.HIDDEN, "fill": 1, "row": 0, "col": 1 },
+      { "stage": CellStateStage.HIDDEN, "fill": 0, "row": 0, "col": 2 }
+    ],
+    [
+      { "stage": CellStateStage.HIDDEN, "fill": 1, "row": 1, "col": 0 },
+      { "stage": CellStateStage.HIDDEN, "fill": 2, "row": 1, "col": 1 },
+      { "stage": CellStateStage.HIDDEN, "fill": 1, "row": 1, "col": 2 }
+    ],
+    [
+      { "stage": CellStateStage.HIDDEN, "fill": 0, "row": 2, "col": 0 },
+      { "stage": CellStateStage.HIDDEN, "fill": 1, "row": 2, "col": 1 },
+      { "stage": CellStateStage.HIDDEN, "fill": 9, "row": 2, "col": 2 }
+    ]
+  ],
+  "begin": 0,
+  "level": 4,
+  "end": 0,
+  "rank": 0,
+  "score": 0,
+  "mines": []
+}
+
+const lostGameState: GameState = {
+  "stage": GameStages.LOST,
+  "board": [
+    [
+      { "stage": CellStateStage.TESTED, "fill": 9, "row": 0, "col": 0 },
+      { "stage": CellStateStage.RELEASED, "fill": 1, "row": 0, "col": 1 },
+      { "stage": CellStateStage.RELEASED, "fill": 0, "row": 0, "col": 2 }
+    ],
+    [
+      { "stage": CellStateStage.RELEASED, "fill": 1, "row": 1, "col": 0 },
+      { "stage": CellStateStage.RELEASED, "fill": 2, "row": 1, "col": 1 },
+      { "stage": CellStateStage.RELEASED, "fill": 1, "row": 1, "col": 2 }
+    ],
+    [
+      { "stage": CellStateStage.RELEASED, "fill": 0, "row": 2, "col": 0 },
+      { "stage": CellStateStage.RELEASED, "fill": 1, "row": 2, "col": 1 },
+      { "stage": CellStateStage.TESTED, "fill": 9, "row": 2, "col": 2 }
+    ]
+  ],
+  "begin": 1745517111606,
+  "level": 4,
+  "end": 1745517111606,
+  "rank": 0,
+  "score": 0,
+  "mines": []
+}
+
+const wonGameState: GameState = {
+  "stage": GameStages.WON,
+  "board": [
+    [
+      { "stage": CellStateStage.RELEASED, "fill": 9, "row": 0, "col": 0 },
+      { "stage": CellStateStage.RELEASED, "fill": 1, "row": 0, "col": 1 },
+      { "stage": CellStateStage.TESTED, "fill": 0, "row": 0, "col": 2 }
+    ],
+    [
+      { "stage": CellStateStage.RELEASED, "fill": 1, "row": 1, "col": 0 },
+      { "stage": CellStateStage.RELEASED, "fill": 2, "row": 1, "col": 1 },
+      { "stage": CellStateStage.RELEASED, "fill": 1, "row": 1, "col": 2 }
+    ],
+    [
+      { "stage": CellStateStage.TESTED, "fill": 0, "row": 2, "col": 0 },
+      { "stage": CellStateStage.RELEASED, "fill": 1, "row": 2, "col": 1 },
+      { "stage": CellStateStage.RELEASED, "fill": 9, "row": 2, "col": 2 }
+    ]
+  ],
+  "begin": 1745518136299,
+  "level": 0,
+  "end": 1745518137216,
+  "rank": 21,
+  "score": 5944,
+  "mines": []
+}
+
+export { newGameState, lostGameState, wonGameState }
---
+++ src/components/game/reducers/replay.test.ts
@@ -0,0 +1,21 @@
+import replayReducer from './replay'
+import { newGameState, wonGameState } from '../../../__mocks__/game-states'
+import { GameState, CellState } from '../../../common/game-types'
+
+test('Whether new or recreated, the cell-values are the same', () => {
+  let findEqualValues = true;
+  const recreatedGameState: GameState = replayReducer(wonGameState)
+
+  newGameState.board.forEach((row, r) =>
+    row.forEach((cell: CellState, c) => {
+      const recreatedCell = recreatedGameState.board[r][c]
+      findEqualValues = findEqualValues
+        && cell.stage === recreatedCell.stage
+        && cell.fill === recreatedCell.fill
+        && cell.row === recreatedCell.row
+        && cell.col === recreatedCell.col
+    })
+  )
+
+  expect(findEqualValues).toBe(true)
+})
