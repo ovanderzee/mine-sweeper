5d841a486778388070bffad4d1a3cf288d73e804
Onno van der Zee <hastalavista@dds.nl>
2025-04-30 00:18:40 +0200

    scale shield text

--- src/components/UI/Shield.tsx
+++ src/components/UI/Shield.tsx
@@ -2,14 +2,26 @@ type ColorSet = 'blue' | 'gold' | 'silver' | 'bronze'
 
 interface ShieldProps {
   colorSet: ColorSet,
-  text?: string | number
+  text: string
 }
 
 const Shield = (props: ShieldProps) => {
+  let fontSizeAdjust
+  switch(props.text.length) {
+    case 1: fontSizeAdjust = '125%'; break
+    case 2: fontSizeAdjust = '100%'; break
+    case 3: fontSizeAdjust = '75%'; break
+    default: fontSizeAdjust = '0%'
+  }
+
   return (
     <svg className={`shield ${props.colorSet}`}>
         <use href={`#shield_${props.colorSet}`} />
-        {props?.text && <text x="50%" y="50%">{props.text}</text>}
+        {props?.text &&
+          <text
+            x="50%" y="50%"
+            style={{'fontSize': fontSizeAdjust}}
+          >{props.text}</text>}
     </svg>
   )
 }
@@ -17,7 +29,7 @@ const Shield = (props: ShieldProps) => {
 const ShieldByRank = (props: {rank: number; }) => {
   const colorSetMap: ColorSet[] = ['blue', 'gold', 'silver', 'bronze']
   const colorSet = colorSetMap[props.rank] || 'blue'
-  return <Shield colorSet={colorSet} text={props.rank} />
+  return <Shield colorSet={colorSet} text={`${props.rank}`} />
 }
 
 export default Shield
