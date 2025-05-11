0a4b57260b1cd709b15d0e11dff05a45b7e66c9f
Onno van der Zee <hastalavista@dds.nl>
2025-05-11 22:31:40 +0200

    remove redundant pointerMove handler

--- src/common/functions.ts
+++ src/common/functions.ts
@@ -1,11 +1,5 @@
 import React from 'react'
 
-export const aspectualInside = (start: number, size: number, current: number): boolean => {
-  // insideness 0 - 1 when inside
-  const insideness = (current - start) / size
-  return insideness >= 0 && insideness <= 1
-}
-
 export const preventReloadByEnter = (event: React.KeyboardEvent) => {
   if (event.key === 'Enter') {
     event.preventDefault()
