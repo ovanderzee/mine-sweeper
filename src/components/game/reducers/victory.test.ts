18ecf60b9b1d14dfdd42200de4db764072d7db51
Onno van der Zee <hastalavista@dds.nl>
2025-06-06 10:19:35 +0200

    test victory reducer

---
+++ src/components/game/reducers/victory.test.ts
@@ -0,0 +1,22 @@
+import victoryReducer from './victory'
+import storage from '../../../common/storage'
+import { microConfig } from '../../../__mocks__/configs'
+import { wonGameState } from '../../../__mocks__/game-states'
+
+describe('victoryReducer makes up scores', () => {
+  test('should persist scores', () => {
+    const initialScores = storage.scores
+
+    victoryReducer(wonGameState, microConfig)
+    const updatedScores = storage.scores
+
+    expect(updatedScores.length).toBe(initialScores.length + 1)
+  })
+
+  test('should add concise victory info to game state', () => {
+    const victoryState = victoryReducer(wonGameState, microConfig)
+
+    expect(victoryState.rank).toBeTruthy()
+    expect(victoryState.score).toBeTruthy()
+  })
+});
