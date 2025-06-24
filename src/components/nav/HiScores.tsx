62b2e0d1c02d8aac1a3e05ae1f8a6fd6c35f762e
Onno van der Zee <hastalavista@dds.nl>
2025-06-24 19:18:38 +0200

    show mines - flags instead of mines

--- src/components/nav/HiScores.tsx
+++ src/components/nav/HiScores.tsx
@@ -2,8 +2,25 @@ import { useContext } from 'react'
 import PageContext from '../../store/page-context'
 import HallOfFame from '../meta/HallOfFame'
 import { ChampionsPodium } from '../UI/Symbols'
+import { CellState } from '../../common/game-types'
 
-const HiScores = (props: { label?: string; }) => {
+interface HiScoresProps {
+  board?: CellState[][]
+}
+
+export const MinesMinusFlags = (props: HiScoresProps) => {
+  const flatBoard = props.board!.flat()
+  const mineCount = flatBoard.filter(c => c.fill > 8).length
+  const flagCount = flatBoard.filter(c => c.locked).length
+  let count = mineCount - flagCount
+  if (count) {
+    return <span>{count}&times;</span>
+  } else {
+    return <span>&mdash;</span>
+  }
+}
+
+const HiScores = (props: HiScoresProps) => {
   const pageCtx = useContext(PageContext)
   const text = pageCtx.text
 
@@ -11,7 +28,7 @@ const HiScores = (props: { label?: string; }) => {
 
   return (
     <button type="button" title={text.nav['Hall of Fame']} onClick={showHandler}>
-      {props.label ? props.label : <ChampionsPodium />}
+      {props.board ? <MinesMinusFlags board={props.board} /> : <ChampionsPodium />}
     </button>
   )
 }
