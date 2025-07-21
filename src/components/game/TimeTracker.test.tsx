648521c252a75639c18ed5775814e1b45d734442
Onno van der Zee <hastalavista@dds.nl>
2025-07-21 12:35:57 +0200

    new analog clock + tests

--- src/components/game/TimeTracker.test.tsx
+++ src/components/game/TimeTracker.test.tsx
@@ -1,15 +1,12 @@
 import { act } from '@testing-library/react'
 import TimeTracker from './TimeTracker'
+import { ClockTypes } from './../../common/app-types'
+import storage from './../../common/storage'
 import { microConfig } from './../../__mocks__/configs'
 import { newGameState, playingGameState, lostGameState } from './../../__mocks__/game-states'
 import { renderInProvider } from './../../__mocks__/render-helpers'
-import storage from './../../common/storage'
 
 describe('TimeTracker', () => {
-  storage.config = microConfig
-  const getMinutesLapsed = () => Number(document.querySelector('#minute')?.textContent)
-  const getSecondsLapsed = () => Number(document.querySelector('#second')?.textContent)
-
   beforeEach(() => {
     act(() => {
       jest.useFakeTimers({timerLimit: 5000})
@@ -18,50 +15,83 @@ describe('TimeTracker', () => {
 
   afterEach(() => {
     act(() => {
-    jest.useRealTimers()
+      jest.useRealTimers()
     })
   })
 
-  it('should not start before game is played', () => {
-    renderInProvider(<TimeTracker game={newGameState} />)
-    expect(getMinutesLapsed()).toBe(0)
-    expect(getSecondsLapsed()).toBe(0)
+  describe('digital clock', () => {
+    const getMinutesLapsed = () => Number(document.querySelector('#minute')?.textContent)
+    const getSecondsLapsed = () => Number(document.querySelector('#second')?.textContent)
 
-    // and stay the same
-    act(() => {
-      jest.advanceTimersByTime(200)
+    beforeEach(() => {
+      storage.config = microConfig
     })
-    expect(getMinutesLapsed()).toBe(0)
-    expect(getSecondsLapsed()).toBe(0)
+
+    it('should not start before game is played', () => {
+      renderInProvider(<TimeTracker game={newGameState} />)
+      expect(getMinutesLapsed()).toBe(0)
+      expect(getSecondsLapsed()).toBe(0)
+
+      // and stay the same
+      act(() => {
+        jest.advanceTimersByTime(200)
+      })
+      expect(getMinutesLapsed()).toBe(0)
+      expect(getSecondsLapsed()).toBe(0)
+    })
+
+    it('should track advancing time when game is played', () => {
+      const playing = {...playingGameState}
+      renderInProvider(<TimeTracker game={playing} />)
+      const minutes = getMinutesLapsed()
+      const seconds = getSecondsLapsed()
+
+      const advanceSecs = 45
+      act(() => {
+        jest.advanceTimersByTime(advanceSecs * 1000)
+      })
+      const newSeconds = seconds + advanceSecs
+      expect(getMinutesLapsed()).toBe(newSeconds < 60 ? minutes : minutes + 1)
+      expect(getSecondsLapsed()).toBe(newSeconds % 60)
+    })
+
+    it('should not advance time when game has ended', () => {
+      renderInProvider(<TimeTracker game={lostGameState} />)
+      const minutes = 3
+      const seconds = 50
+      expect(getMinutesLapsed()).toBe(minutes)
+      expect(getSecondsLapsed()).toBe(seconds)
+
+      // and stay the same
+      act(() => {
+        jest.advanceTimersByTime(200)
+      })
+      expect(getMinutesLapsed()).toBe(minutes)
+      expect(getSecondsLapsed()).toBe(seconds)
+    })
   })
 
-  it('should track advancing time when game is played', () => {
-    const playing = {...playingGameState}
-    renderInProvider(<TimeTracker game={playing} />)
-    const minutes = getMinutesLapsed()
-    const seconds = getSecondsLapsed()
-
-    const advanceSecs = 45
-    act(() => {
-      jest.advanceTimersByTime(advanceSecs * 1000)
+  describe('analog clock', () => {
+    beforeEach(() => {
+      storage.config = {...microConfig, CLOCK_TYPE: ClockTypes.ANALOG}
     })
-    const newSeconds = seconds + advanceSecs
-    expect(getMinutesLapsed()).toBe(newSeconds < 60 ? minutes : minutes + 1)
-    expect(getSecondsLapsed()).toBe(newSeconds % 60)
+
+    it('should rotate hands', () => {
+      renderInProvider(<TimeTracker game={lostGameState} />)
+      // stopped at 3 minutes and 50 seconds
+      const minuteHand = document.querySelector('#minutes') as SVGElement
+      const secondHand = document.querySelector('#seconds') as SVGElement
+      expect(minuteHand.style.transform).toBe('rotate(90deg)')
+      expect(secondHand.style.transform).toBe('rotate(300deg)')
+    })
   })
 
-  it('should not advance time when game has ended', () => {
-    renderInProvider(<TimeTracker game={lostGameState} />)
-    const minutes = 3
-    const seconds = 50
-    expect(getMinutesLapsed()).toBe(minutes)
-    expect(getSecondsLapsed()).toBe(seconds)
-
-    // and stay the same
-    act(() => {
-      jest.advanceTimersByTime(200)
+  describe('no clock', () => {
+    it('should not appear', () => {
+      storage.config = {...microConfig, CLOCK_TYPE: ClockTypes.NONE}
+      renderInProvider(<TimeTracker game={lostGameState} />)
+      const timeTracker = document.querySelector('.time-tracker')
+      expect(timeTracker).not.toBeInTheDocument()
     })
-    expect(getMinutesLapsed()).toBe(minutes)
-    expect(getSecondsLapsed()).toBe(seconds)
   })
 })
--- src/components/game/TimeTracker.tsx
+++ src/components/game/TimeTracker.tsx
@@ -47,17 +47,29 @@ const TimeTracker = (props: TimeTrackerProps) => {
   const padded = (nr: number) => nr.toString().padStart(2, "0")
 
   const analogClock = (
-    <section className="time-tracker analogue">
-      <div id="minute" data-minute={min}
-        style={{transform: `rotate( ${(min % 12) * 360 / 12}deg )`}}
-      ></div>
-      <div id="second" data-second={sec}
-        style={{transform: `rotate( ${(sec % 60) * 360 / 60}deg )`}}
-      ></div>
-    </section>
+    <svg className="time-tracker analogue"
+      width="100"
+      height="100"
+      viewBox="-1 -1 102 102"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g>
+        <path id="minutes"
+          d="m 50,50 c 0,0 2,-28.978965 2,-30 0,-1.021035 -0.895431,-2 -2,-2 -1.104569,0 -2,1.041465 -2,2 0,0.958535 2,30 2,30 z"
+          style={{transform: `rotate(${(min % 12) * 360 / 12}deg)`}}
+        />
+        <path id="seconds"
+          d="m 50,50 c 0,0 2,-38.978965 2,-40 0,-1.0210348 -0.895431,-2 -2,-2 -1.104569,0 -2,1.0414652 -2,2 0,0.958535 2,40 2,40 z"
+          style={{transform: `rotate(${(sec % 60) * 360 / 60}deg)`}}
+        />
+        <circle
+          cx="50"
+          cy="50"
+          r="50"
+        />
+      </g>
+    </svg>
   )
-
-// analog als svg (en wijzers vloeiend laten draainen )
 
   const digitalClock = (
     <section className="time-tracker digital" title={text.game['playtime']}>
