f47b4875abf97d204feb36dabd57d1e94b1b8ef6
Onno van der Zee <hastalavista@dds.nl>
2025-08-14 12:29:04 +0200

    formatting, styling

--- src/components/meta/HallOfFame.tsx
+++ src/components/meta/HallOfFame.tsx
@@ -7,6 +7,7 @@ import GoBack from '../nav/GoBack'
 import { ShieldByRank } from '../UI/Shield'
 import { ScoreItem } from '../../common/game-types'
 import storage from '../../common/storage'
+import { precise } from '../game/scoring'
 import './Meta.css'
 import './HallOfFame.css'
 
@@ -19,42 +20,50 @@ const HallOfFame = () => {
   const latest = [...rawScores].sort((a:ScoreItem, b:ScoreItem) => b.date - a.date)[0]
 
   const [scores, setScores] = useState(rawScores)
+  const [sortLabel, setSortLabel] = useState('points')
 
   const eraseScores = () => {
     setScores(storage.scores)
   }
 
   const sortByPoints = () => {
+    setSortLabel('points')
     const byPoints = (a:ScoreItem, b:ScoreItem) => b.score.points - a.score.points
     setScores(rawScores.sort(byPoints))
   }
 
   const sortByEfficiency = () => {
+    setSortLabel('efficiency')
     const byEfficiency = (a:ScoreItem, b:ScoreItem) => b.score.efficiency - a.score.efficiency
     setScores(rawScores.sort(byEfficiency))
   }
 
   const sortBySpeed = () => {
+    setSortLabel('speed')
     const bySpeed = (a:ScoreItem, b:ScoreItem) => b.score.speed - a.score.speed
     setScores(rawScores.sort(bySpeed))
   }
 
   const sortByMines = () => {
+    setSortLabel('mines')
     const byMines = (a:ScoreItem, b:ScoreItem) => a.game.mines - b.game.mines
     setScores(rawScores.sort(byMines))
   }
 
   const sortByCells = () => {
+    setSortLabel('cells')
     const byCells = (a:ScoreItem, b:ScoreItem) => a.game.cells - b.game.cells
     setScores(rawScores.sort(byCells))
   }
 
   const sortByMoves = () => {
+    setSortLabel('moves')
     const byMoves = (a:ScoreItem, b:ScoreItem) => a.play.moves - b.play.moves
     setScores(rawScores.sort(byMoves))
   }
 
   const sortByDuration = () => {
+    setSortLabel('duration')
     const byDuration = (a:ScoreItem, b:ScoreItem) => a.play.duration - b.play.duration
     setScores(rawScores.sort(byDuration))
   }
@@ -63,9 +72,9 @@ const HallOfFame = () => {
     <article>
       <h2>{text.nav['Hall of Fame']}</h2>
       <ol>
-        <li>
+        <li className={`legend ${sortLabel}`}>
           <footer>
-            <a className="score" onClick={sortByPoints}>{text.fame['score']}</a>
+            <a className="points" onClick={sortByPoints}>{text.fame['points']}</a>
             <div className="efficiency-speed">
               <a className="efficiency" onClick={sortByEfficiency}>{text.fame['efficiency']}</a><br />
               /<a className="speed" onClick={sortBySpeed}>{text.fame['speed']}</a>
@@ -104,8 +113,8 @@ const HallOfFame = () => {
               </h4>
             </header>
             <footer>
-              <div className="score">{log.score.points}</div>
-              <div className="efficiency-speed">{log.score.efficiency}&thinsp;/&thinsp;{log.score.speed}</div>
+              <div className="points">{log.score.points}</div>
+              <div className="efficiency-speed">{precise(log.score.efficiency, 2)}&thinsp;/&thinsp;{precise(log.score.speed, 2)}</div>
               <div className="mines-cells">{log.game.mines}&thinsp;/&thinsp;{log.game.cells}</div>
               <div className="moves">{log.game.effort.least}&#8239;&lt;&#8239;<b>{log.play.moves}</b>&#8239;&lt;&#8239;{log.game.effort.most} </div>
               <div className="duration">{Math.round(log.play.duration)}s</div>
