0f7c312d1791ca825eb8fb9ff1a8d7715f495d65
Onno van der Zee <hastalavista@dds.nl>
2025-09-29 21:18:22 +0200

    open game in fullscreen mode

--- src/components/tips/BoardFitSymbols.tsx
+++ src/components/tips/BoardFitSymbols.tsx
@@ -17,5 +17,24 @@ export const BoardFitSymbols = () => (
       <path opacity=".25" d="M 0,0 h 13 v 18 h -13 Z"/>
       <use href={`#plain-reset`} width="10" height="10" transform="translate(1,2)"/>
     </symbol>
+
+    <symbol id="to-fullscreen" fill="currentcolor"  viewBox="0 0 18 18">
+      <rect x="1" y="1" width="16" height="16" stroke="none" opacity="0.125" />
+      <g fill="none" stroke="currentcolor" strokeWidth="1.5">
+        <path d="M 1,6 v -5 h 5 m -5,0 l 5,5" />
+        <path d="M 12,1 h 5 v 5 m 0,-5 l -5,5" />
+        <path d="M 17,12 v 5 h -5 m 5,0 l -5,-5" />
+        <path d="M 6,17 h -5 v -5 m 0,5 l 5,-5" />
+      </g>
+    </symbol>
+    <symbol id="to-window" fill="currentcolor"  viewBox="0 0 18 18">
+      <rect x="5" y="5" width="8" height="8" stroke="none" opacity="0.125" />
+      <g fill="none" stroke="currentcolor" strokeWidth="1.5">
+        <path d="M 12,17 v -5 h 5 m -5,0 l 5,5" />
+        <path d="M 1,12 h 5 v 5 m 0,-5 l -5,5" />
+        <path d="M 6,1 v 5 h -5 m 5,0 l -5,-5" />
+        <path d="M 17,6 h -5 v -5 m 0,5 l 5,-5" />
+      </g>
+    </symbol>
   </svg>
 )
--- src/components/tips/BoardFitter.tsx
+++ src/components/tips/BoardFitter.tsx
@@ -2,22 +2,60 @@ import { useContext } from 'react'
 import PageContext from '../../store/page-context'
 import './Tips.css'
 
+const enterFullscreen = async (elem: HTMLElement): Promise<void> => {
+  if (elem.requestFullscreen) {elem.requestFullscreen()}
+  // @ts-ignore
+  else if (elem?.mozRequestFullScreen) {elem.mozRequestFullScreen()}
+  // @ts-ignore
+  else if (elem?.msRequestFullscreen) {elem.msRequestFullscreen()}
+  // @ts-ignore
+  else if (elem?.webkitRequestFullscreen) {elem.webkitRequestFullscreen()}
+}
+
+const exitFullscreen = async (): Promise<void> => {
+  if (document?.exitFullscreen) {document.exitFullscreen()}
+  // @ts-ignore}
+  else if (document?.mozCancelFullScreen) {document.mozCancelFullScreen()}
+  // @ts-ignore
+  else if (document?.msExitFullscreen) {document.msExitFullscreen()}
+  // @ts-ignore
+  else if (document?.webkitExitFullscreen) {document.webkitExitFullscreen()}
+}
+
 const BoardFitter = () => {
   const pageCtx = useContext(PageContext)
   const { text } = pageCtx
 
   return (
     <section id="board-fitter" className="tip">
-      <button id="contain" type="button" title={text.tips['Contain all cells']}>
-        <svg><use href={`#contain-view`} /></svg>
+      <button id="contain-fit" type="button" title={text.tips['Contain all cells']}>
+        <svg><use href="#contain-view" /></svg>
       </button>
-      &nbsp;
-      <button id="cover" type="button" title={text.tips['Cover with cells']}>
-        <svg><use href={`#cover-view`} /></svg>
+
+      <button id="cover-fit" type="button" title={text.tips['Cover with cells']}>
+        <svg><use href="#cover-view" /></svg>
       </button>
-      &nbsp;
+
       <button id="reset-fit" type="button" title={text.tips['Revert screen']}>
-        <svg><use href={`#revert-view`} /></svg>
+        <svg><use href="#revert-view" /></svg>
+      </button>
+
+      <button id="fullscreen-mode" type="button"
+        onClick={() => {
+          var playground = window.document.querySelector('#playground') as HTMLElement
+          enterFullscreen(playground)
+            .catch((err: Error) => {
+              alert(`Error entering fullscreen mode: ${err.message} (${err.name})`)
+            })
+        }}
+      >
+        <svg><use href="#to-fullscreen" /></svg>
+      </button>
+
+      <button id="window-mode" type="button"
+        onClick={exitFullscreen}
+      >
+        <svg><use href="#to-window" /></svg>
       </button>
     </section>
   )
