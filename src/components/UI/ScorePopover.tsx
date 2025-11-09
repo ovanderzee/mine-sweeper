47947bec9942af38811a8efd9f1731512473f72c
Onno van der Zee <hastalavista@dds.nl>
2025-11-09 15:12:16 +0100

    detailed score data in popover

--- src/components/UI/ScorePopover.tsx
+++ src/components/UI/ScorePopover.tsx
@@ -1,11 +1,22 @@
+import { useContext } from 'react'
+import PageContext from '../../store/page-context'
+
 import { ScoreItem } from '../../common/game.d'
 import { ShieldByRank } from './Shield'
+import { precise } from '../game/scoring'
+import './ScorePopover.css'
 
 const ScorePopover = (props: {score: ScoreItem | null}) => {
+  const pageCtx = useContext(PageContext)
+  const text = pageCtx.text
   const log = props.score
 
-  return log && (
-    <li
+  if (!log) return
+
+  const loggedDate = new Date(log.date)
+
+  return (
+    <figure
       className={`${log.rank <= 10 ? 'super' : ''} latest`}
       key={`${log.rank}_${log.score.points}`}
     >
@@ -13,13 +24,76 @@ const ScorePopover = (props: {score: ScoreItem | null}) => {
         <h2 className="rank">
           {log.rank <= 10 ? <ShieldByRank rank={log.rank} /> : log.rank}
         </h2>
+        <h3 className="user">{log.user}</h3>
+        <h4 className="date" data-date={log.date}>
+          {loggedDate.toLocaleDateString()}<br/>
+          {loggedDate.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}
+        </h4>
       </header>
+      <article>
+        <section className="group">
+          <h5>{text.fame['result']}</h5>
+          <div className="unit">
+            <small>{text.fame['efficiency']}</small>
+            <span className="efficiency">{precise(log.score.efficiency, 3)}</span>
+          </div>
+          <big>&times;</big>
+          <div className="unit">
+            <small>{text.fame['speed']}</small>
+            <span className="speed">{precise(log.score.speed, 3)}/s</span>
+          </div>
+          <big>&rarr;</big>
+          <div className="unit">
+            <small>{text.fame['points']}</small>
+            <span className="points">{log.score.points}</span>
+          </div>
+        </section>
+
+        <section className="group">
+          <h5>{text.fame['game-play']}</h5>
+          <div className="unit">
+            <small>{text.fame['moves']}</small>
+            <span className="moves">{log.play.moves}</span>
+          </div>
+          <big>|</big>
+          <div className="unit">
+            <small>{text.fame['duration']}</small>
+            <span className="duration">{precise(log.play.duration, 3)}s</span>
+          </div>
+        </section>
+
+        <section className="group">
+          <h5>{text.fame['characteristics']}</h5>
+          <div className="unit">
+            <small>{text.fame['effort']}</small>
+            <span>{log.game.effort.least} - {log.game.effort.most}</span>
+          </div>
+        </section>
+
+        <section className="group">
+          <h5>{text.fame['configuration']}</h5>
+          <div className="unit">
+            <small>{text.fame['level']}</small>
+            <span className="level">{log.level}</span>
+          </div>
+          <big>&</big>
+          <div className="unit">
+            <small>{text.fame['cells']}</small>
+            <span className="cells">{log.game.cells}</span>
+          </div>
+          <big style={{transform: 'scaley(1.25)'}}>&rarr;</big>
+          <div className="unit">
+            <small>{text.fame['mines']}</small>
+            <span className="mines">{log.game.mines}</span>
+          </div>
+        </section>
+      </article>
       <footer>
         <button type="button"
           onClick={() => alert(`rank ${log.rank}`)}
         >{log.user}</button>
       </footer>
-    </li>
+    </figure>
   )
 
 }
