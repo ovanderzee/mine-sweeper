2742e7d73354fce319b3cd75668d0a6a1bddb9a7
Onno van der Zee <hastalavista@dds.nl>
2025-11-16 14:19:22 +0100

    all 38 files + 1 skip, 223 existing tests + 9 skipped passed, remove example

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import * as child from 'child_process';
-import { webdriverio } from '@vitest/browser-webdriverio'
 
 const commitHash = child.execSync('git rev-parse --short HEAD').toString().trim()
 
@@ -17,56 +16,20 @@ export default defineConfig({
     host: '0.0.0.0',
   },
   test: {
-    projects: [
-      {
-        test: {
-          name: 'unit',
-          globals: true,
-          hideSkippedTests: true,
-          environment: 'jsdom', // use 'node' for backend
-          // .spec. files will be moved to browser tests ( .aat. )
-          include: ['src/**/*.{test,spec}.ts'],
-          coverage: {
-            include: ['src'],
-            exclude: [
-              '**/.DS_Store',
-              '**/*.d.ts',
-              'src/main.tsx',
-              'src/components/game/GameCellDemo.tsx',
-              'src/__mocks__/*'
-            ],
-            reporter: ['text', 'html'],
-          },
-          setupFiles: ['./src/vitest.setup.ts'],
-        },
-      },
-      {
-        test: {
-          name: 'browser',
-//           globals: true,
-          hideSkippedTests: true,
-//           environment: 'jsdom', // use 'node' for backend
-          include: ['src/**/*.aat.ts'],
-//           coverage: {
-//             exclude: [
-//               '**/*.d.ts',
-//               'src/main.tsx',
-//               'src/components/game/GameCellDemo.tsx',
-//               'src/__mocks__/*'
-//             ],
-//             reporter: ['text', 'html'],
-//           },
-//   ??        setupFiles: ['./src/vitest.setup.ts'],
-          browser: {
-            provider: webdriverio(),
-            enabled: true,
-            // at least one instance is required
-            instances: [
-              { browser: 'chrome' },
-            ],
-          },
-        },
-      },
-    ]
+    globals: true,
+    hideSkippedTests: true,
+    environment: 'jsdom', // use 'node' for backend
+    coverage: {
+      include: ['src'],
+      exclude: [
+        '**/.DS_Store',
+        '**/*.d.ts',
+        'src/main.tsx',
+        'src/components/game/GameCellDemo.tsx',
+        'src/__mocks__/*'
+      ],
+      reporter: ['text', 'html'],
+    },
+    setupFiles: ['./src/vitest.setup.ts'],
   },
 })
