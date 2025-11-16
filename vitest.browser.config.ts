a135d5a10c29f66ad6bca40dce67047b415123a5
Onno van der Zee <hastalavista@dds.nl>
2025-11-16 11:37:04 +0100

    npx vitest init browser

---
+++ vitest.browser.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vitest/config'
+import { webdriverio } from '@vitest/browser-webdriverio'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    browser: {
+      enabled: true,
+      provider: webdriverio(),
+      // https://vitest.dev/guide/browser/webdriverio
+      instances: [
+      { browser: 'chrome' },
+      { browser: 'firefox' },
+      { browser: 'safari' },
+      ],
+    },
+  },
+})
