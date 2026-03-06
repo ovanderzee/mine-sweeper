12ade094a87b3f68114f7b1ff2847d2c05e786b8
Onno van der Zee <hastalavista@dds.nl>
2026-03-06 21:22:53 +0100

    split config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as child from 'child_process';
-import { webdriverio } from '@vitest/browser-webdriverio'
 
 const commitHash = child.execSync('git rev-parse --short HEAD').toString().trim()
 
@@ -15,45 +14,5 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',
-  },
-  test: {
-    projects: [
-      {
-  test: {
-    name: 'unit',
-    globals: true,
-    hideSkippedTests: true,
-    environment: 'jsdom', // use 'node' for backend
-    // @ts-expect-error // error TS2769: No overload matches this call
-    coverage: {
-      include: ['src'],
-      reporter: ['text', 'html'],
-    },
-    setupFiles: ['./src/vitest.setup.ts'],
-  },
-      },
-      {
-  test: {
-    include: ['src/**/*.aat.tsx'],
-    name: 'aat',
-    browser: {
-      enabled: true,
-      provider: webdriverio(),
-      headless: true,
-      viewport: { width: 800, height: 600 },
-      // https://vitest.dev/guide/browser/webdriverio
-      instances: [
-        { browser: 'chrome' },
-      ],
-    },
-    printConsoleTrace: true,
-    setupFiles: ['./src/vitest.set-browser.ts'],
-  },
-      }
-    ]
-  },
-  cacheDir: '.vite-cache', // to persist optimisations
-  optimizeDeps: {
-    include: ['react/jsx-dev-runtime']
   }
 })
