29ea5c4eb684685db24a48f22e6a9e20cba1c6e4
Onno van der Zee <hastalavista@dds.nl>
2026-07-02 14:36:38 +0200

    refactor configure component

--- src/components/meta/Configure.tsx
+++ src/components/meta/Configure.tsx
@@ -21,6 +21,13 @@ function Configure() {
     storage.eraseGame()
   }
 
+  const cellCount = Math.pow(config.BOARD_SIZE, 2)
+  const cellsPerMine = Math.round(GAME_DIVISOR * 10 / config.GAME_LEVEL) / 10
+
+  /**
+    GAME - PLAY SETTINGS
+  */
+
   const changeBoardSizeHandler = (event: React.ChangeEvent) => {
     exitCurrentGame()
     const ctrl = event.target as HTMLInputElement
@@ -30,6 +37,24 @@ function Configure() {
     )
     pageCtx.configure({ BOARD_SIZE: value })
   }
+
+  const changeBoardSizeContent = (
+    <div className="field">
+      <label htmlFor="size">{text.settings['Size Gameboard']}</label>
+      <div>
+        <em>{text.settings['%n cells'].replace('%n', cellCount.toString())}</em>
+        <input
+          id="size"
+          type="range"
+          value={config.BOARD_SIZE}
+          min={RANGES.SIZE.min}
+          max={RANGES.SIZE.max}
+          onChange={changeBoardSizeHandler}
+          onClick={changeBoardSizeHandler as unknown as React.MouseEventHandler<HTMLInputElement>}
+        />
+      </div>
+    </div>
+  )
 
   const changeGameLevelHandler = (event: React.ChangeEvent) => {
     exitCurrentGame()
@@ -41,8 +66,24 @@ function Configure() {
     pageCtx.configure({ GAME_LEVEL: value })
   }
 
-  const cellCount = Math.pow(config.BOARD_SIZE, 2)
-  const cellsPerMine = Math.round(GAME_DIVISOR * 10 / config.GAME_LEVEL) / 10
+  const changeGameLevelContent = (
+    <div className="field">
+      <label htmlFor="level">{text.settings['Gamelevel']}</label>
+      <div>
+        <em>{text.settings['one mine to %n cells'].replace('%n', cellsPerMine.toString())}</em>
+        <input
+          id="level"
+          type="range"
+          value={config.GAME_LEVEL}
+          min={RANGES.LEVEL.min}
+          max={RANGES.LEVEL.max}
+          onChange={changeGameLevelHandler}
+          onClick={changeGameLevelHandler as unknown as React.MouseEventHandler<HTMLInputElement>}
+        />
+        <em>{text.settings['total %n mines'].replace('%n', config.MINE_COUNT.toString())}</em>
+      </div>
+    </div>
+   )
 
   const changeNormalModeHandler = () => {
     exitCurrentGame()
@@ -59,89 +100,76 @@ function Configure() {
     pageCtx.configure({ PLAY_MODE: PlayMode.SHARP})
   }
 
-  const onbeginContent = (
-    <fieldset id="on-begin-play">
-      <legend>{text.settings['On begin Play']}</legend>
-      <div className="h5 warning">{text.settings['Will destroy current game']}</div>
+  const changePlayModeContent = (
+    <div className="field">
+      <div className="label">{text.settings['Play mode']}</div>
+      <label>
+        <input
+          name="playmode"
+          type="radio"
+          checked={config.PLAY_MODE === PlayMode.NORMAL}
+          onChange={changeNormalModeHandler}
+          onClick={changeNormalModeHandler as unknown as React.MouseEventHandler<HTMLInputElement>}
+        /> &nbsp;
+        {text.settings['Normal']}<br />
+        <em>{text.settings['normal mode']}</em>
+      </label>
 
-      <div className="field">
-        <label htmlFor="size">{text.settings['Size Gameboard']}</label>
-        <div>
-          <em>{text.settings['%n cells'].replace('%n', cellCount.toString())}</em>
-          <input
-            id="size"
-            type="range"
-            value={config.BOARD_SIZE}
-            min={RANGES.SIZE.min}
-            max={RANGES.SIZE.max}
-            onChange={changeBoardSizeHandler}
-            onClick={changeBoardSizeHandler as unknown as React.MouseEventHandler<HTMLInputElement>}
-          />
-        </div>
-      </div>
+      <label>
+        <input
+          name="playmode"
+          type="radio"
+          checked={config.PLAY_MODE === PlayMode.BARE}
+          onChange={changeBareModeHandler}
+          onClick={changeBareModeHandler as unknown as React.MouseEventHandler<HTMLInputElement>}
+        /> &nbsp;
+        {text.settings['Bare']}<br />
+        <em>{text.settings['bare mode']}</em>
+      </label>
 
-      <div className="field">
-        <label htmlFor="level">{text.settings['Gamelevel']}</label>
-        <div>
-          <em>{text.settings['one mine to %n cells'].replace('%n', cellsPerMine.toString())}</em>
-          <input
-            id="level"
-            type="range"
-            value={config.GAME_LEVEL}
-            min={RANGES.LEVEL.min}
-            max={RANGES.LEVEL.max}
-            onChange={changeGameLevelHandler}
-            onClick={changeGameLevelHandler as unknown as React.MouseEventHandler<HTMLInputElement>}
-          />
-          <em>{text.settings['total %n mines'].replace('%n', config.MINE_COUNT.toString())}</em>
-        </div>
-      </div>
-
-      <div className="field">
-        <div className="label">{text.settings['Play mode']}</div>
-        <label>
-          <input
-            name="playmode"
-            type="radio"
-            checked={config.PLAY_MODE === PlayMode.NORMAL}
-            onChange={changeNormalModeHandler}
-            onClick={changeNormalModeHandler as unknown as React.MouseEventHandler<HTMLInputElement>}
-          /> &nbsp;
-          {text.settings['Normal']}<br />
-          <em>{text.settings['normal mode']}</em>
-        </label>
-
-        <label>
-          <input
-            name="playmode"
-            type="radio"
-            checked={config.PLAY_MODE === PlayMode.BARE}
-            onChange={changeBareModeHandler}
-            onClick={changeBareModeHandler as unknown as React.MouseEventHandler<HTMLInputElement>}
-          /> &nbsp;
-          {text.settings['Bare']}<br />
-          <em>{text.settings['bare mode']}</em>
-        </label>
-
-        <label>
-          <input
-            name="play-mode"
-            type="radio"
-            checked={config.PLAY_MODE === PlayMode.SHARP}
-            onChange={changeSharpModeHandler}
-            onClick={changeSharpModeHandler as unknown as React.MouseEventHandler<HTMLInputElement>}
-          /> &nbsp;
-          {text.settings['Sharp']}<br />
-          <em>{text.settings['sharp mode']}</em>
-        </label>
-      </div>
-    </fieldset>
+      <label>
+        <input
+          name="play-mode"
+          type="radio"
+          checked={config.PLAY_MODE === PlayMode.SHARP}
+          onChange={changeSharpModeHandler}
+          onClick={changeSharpModeHandler as unknown as React.MouseEventHandler<HTMLInputElement>}
+        /> &nbsp;
+        {text.settings['Sharp']}<br />
+        <em>{text.settings['sharp mode']}</em>
+      </label>
+    </div>
   )
+
+  /**
+    GENERAL SETTINGS
+  */
 
   const changeLanguageHandler = (event: React.ChangeEvent) => {
     const ctrl = event.target as HTMLSelectElement
     pageCtx.configure({ LANGUAGE: ctrl.value as Languages })
   }
+
+  const changeLanguageContent = (
+    <div className="field">
+      <label htmlFor="language">{text.settings['Translations']}</label>
+      <div>
+        <em>{text.settings['choose your language']}</em>
+        <select
+          id="language"
+          value={config.LANGUAGE}
+          onChange={changeLanguageHandler}
+        >
+          {Object.entries(LanguageTranslation).map(([key, value]) => (
+            <option
+              key={key}
+              value={key}
+            >{value}</option>
+          ))}
+        </select>
+      </div>
+    </div>
+  )
 
   const changeFontSizeHandler = (event: React.ChangeEvent) => {
     const ctrl = event.target as HTMLInputElement
@@ -153,46 +181,22 @@ function Configure() {
     pageCtx.configure({ FONT_SIZE: value })
   }
 
-  const genericContent = (
-    <fieldset id="general-settings">
-      <legend>{text.settings['General Settings']}</legend>
-
-      <div className="field">
-        <label htmlFor="language">{text.settings['Translations']}</label>
-        <div>
-          <em>{text.settings['choose your language']}</em>
-          <select
-            id="language"
-            value={config.LANGUAGE}
-            onChange={changeLanguageHandler}
-          >
-            {Object.entries(LanguageTranslation).map(([key, value]) => (
-              <option
-                key={key}
-                value={key}
-              >{value}</option>
-            ))}
-          </select>
-        </div>
+  const changeFontSizeContent = (
+    <div className="field">
+      <label htmlFor="zoom">{text.settings['Zoom Display']}</label>
+      <div>
+        <em>{text.settings['font-size to %n pixels'].replace('%n', config.FONT_SIZE.toString())}</em>
+        <input
+          id="zoom"
+          type="range"
+          value={config.FONT_SIZE}
+          min={RANGES.FONT.min}
+          max={RANGES.FONT.max}
+          onChange={changeFontSizeHandler}
+          onClick={changeFontSizeHandler as unknown as React.MouseEventHandler<HTMLInputElement>}
+        />
       </div>
-
-      <div className="field">
-        <label htmlFor="zoom">{text.settings['Zoom Display']}</label>
-        <div>
-          <em>{text.settings['font-size to %n pixels'].replace('%n', config.FONT_SIZE.toString())}</em>
-          <input
-            id="zoom"
-            type="range"
-            value={config.FONT_SIZE}
-            min={RANGES.FONT.min}
-            max={RANGES.FONT.max}
-            onChange={changeFontSizeHandler}
-            onClick={changeFontSizeHandler as unknown as React.MouseEventHandler<HTMLInputElement>}
-          />
-        </div>
-      </div>
-
-    </fieldset>
+    </div>
   )
 
   const changePlayerNameHandler = (event: React.ChangeEvent) => {
@@ -200,24 +204,19 @@ function Configure() {
     pageCtx.configure({ PLAYER_NAME: ctrl.value })
   }
 
-  const recordContent = (
-    <fieldset id="record-score">
-      <legend>{text.settings['Record Scores']}</legend>
-
-      <div className="field">
-        <label htmlFor="user">{text.settings['Name in Scores']}</label>
-        <div>
-          <em>{text.settings['type your name']}</em>
-          <input
-            id="user"
-            type="text"
-            value={config.PLAYER_NAME}
-            onChange={changePlayerNameHandler}
-          />
-        </div>
+  const changePlayerNameContent = (
+    <div className="field">
+      <label htmlFor="user">{text.settings['Name in Scores']}</label>
+      <div>
+        <em>{text.settings['type your name']}</em>
+        <input
+          id="user"
+          type="text"
+          value={config.PLAYER_NAME}
+          onChange={changePlayerNameHandler}
+        />
       </div>
-
-    </fieldset>
+    </div>
   )
 
   const configNavigation = (
@@ -238,9 +237,19 @@ function Configure() {
         onKeyDown={(event) => preventReloadByEnter(event)}
       >
         <h1 className="sr-only" id="page-heading">{text.nav['Settings']}</h1>
-        {onbeginContent}
-        {genericContent}
-        {recordContent}
+        <fieldset id="game-play">
+          <legend>{text.settings['On begin Play']}</legend>
+          <div className="h5 warning">{text.settings['Will destroy current game']}</div>
+          {changeBoardSizeContent}
+          {changeGameLevelContent}
+          {changePlayModeContent}
+        </fieldset>
+        <fieldset id="general-settings">
+          <legend>{text.settings['General Settings']}</legend>
+          {changeLanguageContent}
+          {changeFontSizeContent}
+          {changePlayerNameContent}
+        </fieldset>
       </form>
     </article>
     {configNavigation}
