a1b6b8ad0646cbc78d12be53015dae0225382993
Onno van der Zee <hastalavista@dds.nl>
2026-07-10 10:38:41 +0200

    style range-input for webkit

--- src/components/PageRoot.tsx
+++ src/components/PageRoot.tsx
@@ -3,6 +3,7 @@ import { useContext, useEffect, useState } from 'react'
 import Game from './game/Game'
 import PageContext from '../store/page-context'
 import { rootKeyListener } from '../common/functions'
+import { DEFAULTS } from '../common/defaults'
 
 function PageRoot() {
   const pageCtx = useContext(PageContext)
@@ -29,7 +30,7 @@ function PageRoot() {
   return (
     <section
       className="screen"
-      style={{fontSize: `${pageCtx.config.FONT_SIZE}px`}}
+      style={{fontSize: `${pageCtx.config.FONT_SIZE}px`, '--font-size': `${pageCtx.config.FONT_SIZE}px`, '--font-zoom': `${pageCtx.config.FONT_SIZE / DEFAULTS.FONT_SIZE}`}}
     >
       {pageCtx.render}
       {showIntroduction && introLayer}
