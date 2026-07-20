a4c69e6120ec18b0aa5b84e51f4894ea2d32e724
Onno van der Zee <hastalavista@dds.nl>
2026-07-20 10:53:00 +0200

    positioning and texts

--- src/components/UI/Histogram.tsx
+++ src/components/UI/Histogram.tsx
@@ -44,61 +44,65 @@ const Histogram = (props: HistogramProps) => {
 
   const lgdSpace = 100  // legenda left and bottom
   const lgdOver = { x: 0, y: 30 }
-  const pointsSpace = 5 // graph top and right
-  const graphSize = { x: 600, y: 400 }
+  const pointsSpace = { x: 5, y: 10 } // graph top and right
+  const graphSize = { x: 600, y: 100 }
   const axisMax = { x: calcBoundingAxis(max.x), y: calcBoundingAxis(max.y) }
   const dataScale = { x: graphSize.x / axisMax.x, y: graphSize.y / axisMax.y }
 
   const diagramSize = {
-    x: graphSize.x + lgdSpace + pointsSpace + lgdOver.x,
-    y: graphSize.y + lgdSpace + pointsSpace + lgdOver.y
+    x: graphSize.x + lgdSpace + pointsSpace.x + lgdOver.x,
+    y: graphSize.y + lgdSpace + pointsSpace.y + lgdOver.y
   }
-  const crossLegSize = 75
+
+  // @ts-expect-error // error TS6133: 'd' is declared but its value is never read.
+  const xValues = coordinates.map((d, i) =>
+    (i%9) && <text x={dataScale.x * (i + .5)} y="120" textAnchor="middle">{i % 9}</text>
+  )
 
   return (
     <svg
       role="document"
-      className="line-diagram"
-      viewBox={`${lgdSpace * -1} ${pointsSpace * -1} ${diagramSize.x} ${diagramSize.y}`}
+      className="frequency-histogram"
+      viewBox={`${lgdSpace * -1} ${pointsSpace.y * -1} ${diagramSize.x} ${diagramSize.y}`}
       xmlns="http://www.w3.org/2000/svg"
     >
       <g className="legenda">
         <line x1="0" y1={graphSize.y} x2={graphSize.x} y2={graphSize.y} />
-        <text x="0" y={graphSize.y} dx="20" dy={lgdSpace * .9} textAnchor="middle" data-testid="x-parameter"
-          >{text.VAR[props.xParam]} &rarr;</text>
+        <text x={graphSize.x} y={graphSize.y} dy={lgdSpace * .67} textAnchor="end" data-testid="x-parameter"
+          >{text.VAR.surrounding} &rarr;</text>
         <line x1="0" y1="0" x2="0" y2={graphSize.y} />
-        <text x={-graphSize.y} y="0" dx="20" dy={lgdSpace * -.8} transform="rotate(-90)" textAnchor="middle" data-testid="y-parameter"
-          >{text.VAR[props.yParam]} &rarr;</text>
-
-        <text x={graphSize.x} y={graphSize.y} dy="120" textAnchor="end" style={{fontSize: '133%'}}>
-          {coordinates.length} {text.fame['won games']}
+        <text x="0" y="-70" transform="rotate(-90)" textAnchor="end" data-testid="y-parameter"
+          >{text.VAR.occurrences} &rarr;</text>
+        <text x={graphSize.x} y={graphSize.y} dy="120" textAnchor="end">
+          <tspan>{text.VAR.blanks}</tspan>
+          <tspan>{text.VAR.pointers}</tspan>
+          <tspan>{text.VAR.mines}</tspan>
         </text>
       </g>
       <g className="x-axis">
-        // zero
-        <line x1="0" y1={graphSize.y} x2="0" y2={graphSize.y + 20} />
-        <text x="0" y={graphSize.y} dy={lgdSpace * .5} textAnchor="middle">0</text>
-        // max
-        <line x1={graphSize.x} y1={graphSize.y} x2={graphSize.x} y2={graphSize.y + 20} />
-        <text x={graphSize.x} y={graphSize.y} dy={lgdSpace * .5} textAnchor="end">{axisMax.x}</text>
+        {xValues}
       </g>
       <g className="y-axis">
         <line x1="0" y1="0" x2="-20" y2="0" />
-        <text x="0" y="0" dy={lgdSpace * -.4} transform="rotate(-90)" textAnchor="end">{axisMax.y}</text>
+        <text x="-33" y="6" textAnchor="end">{axisMax.y}</text>
         <line x1="0" y1={graphSize.y} x2="-20" y2={graphSize.y} />
-        <text x={-graphSize.y} y="0" dy={lgdSpace * -.4} transform="rotate(-90)" textAnchor="middle">0</text>
+        <text x="-33" y="106" textAnchor="middle">0</text>
       </g>
       <g className="data-points">
         {coordinates.map((d, i) =>
-          <g className={`data-point`} key={`lnd_group_${i}`}
-            transform={`translate(${d.x * dataScale.x}, ${(axisMax.y - d.y) * dataScale.y})`}
+          <g className={`data-point`} key={`lnd_group_${i}`} fill={d.color}
+            transform={`translate(${d.x * dataScale.x}, ${axisMax.y * dataScale.y})`}
           >
-            <path d={`M ${-crossLegSize}, 0 ${crossLegSize}, 0 M 0,${-crossLegSize} 0, ${crossLegSize}`} key={`lnd_path_${i}`} />
-            <circle  cx="0" cy="0" r="3" key={`lnd_circle_${i}`} aria-labelledby={`lnd_title_${i}`}>
+            <path d={`M 0,0 V -100 H ${dataScale.x} V 0 Z`} key={`lnd_back_${i}`} opacity=".4" aria-labelledby={`lnd_title_${i}`}>
               <title id={`lnd_title_${i}`} key={`lnd_title_${i}`}>
-                {`${text.VAR[props.xParam]}: ${d.x}, ${text.VAR[props.yParam]}: ${d.y}`}
+                {`${text.VAR.surrounding}: ${d.x % 9}, ${text.VAR.occurrences}: ${d.y}`}
               </title>
-            </circle>
+            </path>
+            <path d={`M 0,0 V ${-d.y * dataScale.y} H ${dataScale.x} V 0 Z`} key={`lnd_front_${i}`} aria-labelledby={`lnd_title_${i}`}>
+              <title id={`lnd_title_${i}`} key={`lnd_title_${i}`}>
+                {`${text.VAR.surrounding}: ${d.x % 9}, ${text.VAR.occurrences}: ${d.y}`}
+              </title>
+            </path>
           </g>)}
       </g>
 
