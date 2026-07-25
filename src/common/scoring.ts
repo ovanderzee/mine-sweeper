230ce323bdc1779cb223f418b12992de2b5ebf16
Onno van der Zee <hastalavista@dds.nl>
2026-07-25 11:23:46 +0200

    more info on code errors

--- src/common/scoring.ts
+++ src/common/scoring.ts
@@ -144,7 +144,8 @@ export const rebuildGameData = (boardCode: string): {board: CellState[][], confi
   }
 
   if (invalidCode || wrongSize || wrongMineCount || wrongPlayMode) {
-    console.error(`Invalid ${wrongSize ? 'size' : wrongMineCount ? 'mine count' : wrongPlayMode ? 'playmode' : 'code'}`)
+    const report = `Invalid ${wrongSize ? 'size' : wrongMineCount ? 'mine count' : wrongPlayMode ? 'playmode' : 'code'}`
+    console.error(`${report} ${boardCode}`)
     return {board: [[]], config: checkConfig}
   }
 
